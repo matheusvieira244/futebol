e94d7f814680d9543a14dfe19b5d5ac7364b7415
matheusvieira244 <mateusdiasssthe@gmail.com>
2025-06-18 08:51:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function enviar(){
+    alert("Cadastro feito com sucesso!")
+}
+
+//Variavel para pegar o nome do visitante
+var usuario = prompt ("Digite o seu nome")
+
+//Pegar o valor da variavel e imprimir na tela
+document.getElementById('visitante').innerHTML = usuario
